ef6caf60b968f0dbe9051e28272b4a42fc872b18
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-12-21 22:56:36 -0300

    feat: enable root import

---
+++ src/components/Test/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Test() {
+  return <Text>Test</Text>;
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import Test from '~/components/Test';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <View>
+      <Test />
+    </View>
+  );
+}
