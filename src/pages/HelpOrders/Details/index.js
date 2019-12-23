a775dee4ba65a89101c684d3ae875badbeaf7577
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-12-23 17:24:50 -0300

    feat: show help order details

--- src/pages/HelpOrders/Details/index.js
+++ src/pages/HelpOrders/Details/index.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useMemo } from 'react';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import Background from '~/components/Background';
 import PageTitle from '~/components/PageTitle';
 
-import { Container } from './styles';
+import { Container, Box, Header, Title, Time, Content, Answer } from './styles';
 
 export default function Details({ navigation }) {
   const helpOrder = navigation.getParam('helpOrder');
+
+  const formattedTime = useMemo(() => {
+    return formatRelative(parseISO(helpOrder.createdAt), new Date(), {
+      locale: pt,
+    });
+  }, [helpOrder.createdAt]);
+
   return (
     <Background>
       <PageTitle back />
       <Container>
-        <Text>{helpOrder.id}</Text>
+        <Box>
+          <Header>
+            <Title>Pergunta</Title>
+            <Time>{formattedTime}</Time>
+          </Header>
+          <Content>{helpOrder.question}</Content>
+          {helpOrder.answer && (
+            <Answer>
+              <Title>Resposta</Title>
+              <Content>{helpOrder.answer}</Content>
+            </Answer>
+          )}
+        </Box>
       </Container>
     </Background>
   );
--- src/pages/HelpOrders/Details/styles.js
+++ src/pages/HelpOrders/Details/styles.js
@@ -4,3 +4,40 @@ export const Container = styled.SafeAreaView`
   flex: 1;
   margin: 20px 25px 0;
 `;
+
+export const Box = styled.View`
+  background: ${props => props.theme.white};
+  border: 1px solid ${props => props.theme.border};
+  border-radius: 4px;
+  padding: 20px;
+  align-self: stretch;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${props => props.theme.textDark};
+`;
+
+export const Time = styled.Text`
+  text-align: right;
+  font-size: 14px;
+  color: ${props => props.theme.text};
+`;
+
+export const Content = styled.Text`
+  color: ${props => props.theme.text};
+  font-size: 14px;
+  margin-top: 16px;
+`;
+
+export const Answer = styled.View`
+  margin-top: 20px;
+`;
